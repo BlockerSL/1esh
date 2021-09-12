c50a0fc426392317388a84c946f4706ebe093cc1
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-09-12 16:44:10 +0300

    Update Buttons.vue

--- app/src/components/Buttons.vue
+++ app/src/components/Buttons.vue
@@ -10,7 +10,7 @@
       icon="el-icon-view"
       @click="syntaxGuide = !syntaxGuide"
     >
-      {{ syntaxGuide ? "Hide" : "" }} ڕێنوێنی سینتاکس
+      {{ syntaxGuide ? "Hide" : "Show" }} ڕێنوێنی سینتاکس
    </el-button>
  <SyntaxGuide :initialVisibility="syntaxGuide" @visibility="syntaxGuide = $event" />
   </div>
@@ -37,12 +37,3 @@ export default {
     },
   },
 };
-</script>
-<style>
-button{
-  border:1px solid white;
-  border-radius: 15px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-  
-}
-</style>
